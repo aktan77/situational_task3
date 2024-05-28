d2b8f565e7545e680df1fdd70725bc76ae4f0fd6
aktan77 <aktan.asankulov55@gmail.com>
2024-05-28 17:24:02 +0500

    Package and docker part is done.

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+sudo apt update
+sudo apt install -y docker.io docker-compose nginx openssl
+
+# Enable and star docker service
+sudo systemctl enable docker
+sudo systemctl start docker
+
+cd app
+
+# Create requirements.txt file
+cat <<EOF > requirements.txt
+Flask==2.0.1
+EOF
+
+# Create Dockerfile for Flask application
+cat <<EOF > Dockerfile
+FROM python:3.8-slim
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app.py"]
+EOF
+
+# Create Docker Compose file
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  web:
+    build: .
+    ports:
+      - "5000:5000"
+EOF
+
+
+# Build and run Docker container
+sudo docker-compose up -d
